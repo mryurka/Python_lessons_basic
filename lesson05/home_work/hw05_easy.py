96f13aa5149158247cc2f063d23c7917b8078366
Yura <mr.yurka@gmail.com>
2019-05-13 19:22:40 +0700

    Переделал задачи урока 05 уровня easy и сделал normal

--- lesson05/home_work/hw05_easy.py
+++ lesson05/home_work/hw05_easy.py
@@ -7,39 +7,52 @@ import shutil
 # Напишите скрипт, создающий директории dir_1 - dir_9 в папке,
 # из которой запущен данный скрипт.
 # И второй скрипт, удаляющий эти папки.
-FOLDER = os.getcwd()
-
-dir_name = "dir_"
-print("Текущая папка -->", FOLDER)
-for index in range(1, 10):
+def mk_folder(folder=os.getcwd(), dir_name="new_dir"):
+    full_name = os.path.join(folder, dir_name)
     try:
-        os.mkdir(dir_name + str(index))
+        os.mkdir(full_name)
     except FileExistsError:
-        print(f"Ошибка создания! Папка {dir_name + str(index)} уже существует...")
+        return f"Ошибка создания! Папка {dir_name} уже существует..."
     else:
-        print("Создана папка -->", os.path.abspath(dir_name + str(index)))
-print("-" * 50)
+        return "Создана папка -->", os.path.abspath(dir_name)
+
 
 # Задача-2:
 # Напишите скрипт, отображающий папки текущей директории.
-dir_lst = os.listdir(FOLDER)
-print("Содержимое папки -->", FOLDER)
-mark = 1
-for item in dir_lst:
-    if os.path.isdir(os.path.join(FOLDER, item)):
-        print(f"{mark})folder ../", item)
-    else:
-        print(f"{mark})file ---->", item)
-    mark += 1
-print("-" * 50)
+def disp_folder(folder=os.getcwd()):
+    dir_lst = os.listdir(folder)
+    print("Содержимое папки -->", folder)
+    mark = 1
+    for item in dir_lst:
+        if os.path.isdir(os.path.join(folder, item)):
+            print(f"{mark})folder ../", item)
+        else:
+            print(f"{mark})file ---->", item)
+        mark += 1
+
 
 # Задача-3:
 # Напишите скрипт, создающий копию файла, из которого запущен данный скрипт.
-print("Текущая папка -->", FOLDER)
-file = sys.argv[0]
-file_name = os.path.split(file)[1]
-print("Копируется файл -->", file_name)
-shutil.copy(file, f"copy_{file_name}")
-print(f"Файл скопирован, имя копии --> copy_{file_name}")
+def simple_file_copy(folder=os.getcwd()):
+    print("Текущая папка -->", folder)
+    file = sys.argv[0]
+    file_name = os.path.split(file)[1]
+    print("Копируется файл -->", file_name)
+    shutil.copy(file, f"copy_{file_name}")
+    print(f"Файл скопирован, имя копии --> copy_{file_name}")
+
+
+if __name__ == '__main__':
+    print("--- Задача-1 ---")
+    for index in range(1, 10):
+        my_name = 'dir_' + str(index)
+        print(mk_folder(dir_name=my_name))
+    print("-" * 50)
+    print("--- Задача-2 ---")
+    disp_folder()
+    print("-" * 50)
+    print("--- Задача-3 ---")
+    simple_file_copy()
+    print("-" * 50)
 
 
--- lesson05/home_work/hw05_normal.py
+++ lesson05/home_work/hw05_normal.py
@@ -1,3 +1,6 @@
+import os
+import shutil
+
 # Задача-1:
 # Напишите небольшую консольную утилиту,
 # позволяющую работать с папками текущей директории.
@@ -13,3 +16,76 @@
 # Для решения данной задачи используйте алгоритмы из задания easy,
 # оформленные в виде соответствующих функций,
 # и импортированные в данный файл из easy.py
+
+from hw05_easy import disp_folder
+from hw05_easy import mk_folder
+
+
+def game_over():  # command 0
+    return True
+
+
+def goto():  # command 1
+    destination = input('Введите полный путь для перехода и нажмите "Enter": ')
+    try:
+        os.chdir(destination)
+    except FileNotFoundError:
+        print("Ошибка такого каталога не существует, попробуйте ещё раз")
+    else:
+        print("Текущий каталог изменён на:", os.getcwd())
+
+
+def disp():  # command 2
+    disp_folder(os.getcwd())
+
+
+def remove():  # command 3
+    work_dir = os.getcwd()
+    del_dir_name = input("Введите название папки для удаления: ")
+    confirmation = input(f'Вы уверены что хотите удалить папку {del_dir_name} включая все вложенные папки и файлы? \
+Выберите Y если ДА или N если нет и нажмите "Enter" :')
+    if confirmation == 'Y' or confirmation == 'y':
+        try:
+            shutil.rmtree(os.path.join(work_dir, del_dir_name))
+        except FileNotFoundError:
+            print("Ошибка удаления такого каталога не существует, попробуйте ещё раз")
+    else:
+        print('Удаление не подтверждено, для подтверждения нужно выбрать "Y"')
+
+
+def mkdir():  # command 4
+    work_dir = os.getcwd()
+    new_dir_name = input("Введите название новой папки: ")
+    print(mk_folder(work_dir, new_dir_name))
+
+
+
+commands = [game_over, goto, disp, remove, mkdir]  # список из функций для удобства вызова (порядок как в меню!)
+enough = False
+while not enough:
+    # -------------------получаем и очищаем комманду пользователя----------------------------------
+    txt = f"""\nВаш текущий рабочий каталог {os.getcwd()} 
+    Пожалуйста выберете действие от 1 - 5 или 0 для выхода и нажмите "Enter": 
+        0. Выйти из программы
+        1. Перейти в папку
+        2. Просмотреть содержимое текущей папки
+        3. Удалить папку
+        4. Создать папку
+           Ваш выбор --> """
+    input_command = input(txt)
+    try:
+        input_command = int(input_command)
+        if input_command in range(0, 5):
+            com_clear = input_command
+        else:
+            com_clear = ("Input error: value not in options range (from 0 to 4)", input_command)
+    except ValueError:
+        com_clear = ("Input error: Value is not correct", input_command)
+    # -----------------Вызываем функции в зависимости от комманды---------------------------------
+    if type(com_clear) is int and com_clear != 0:
+        commands[com_clear]()
+    elif type(com_clear) is int and com_clear == 0:
+        enough = True
+    else:
+        print(com_clear)
+
